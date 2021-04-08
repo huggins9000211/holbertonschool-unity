7970543b4d8c7f22f827f254b37d3037b1a6635b
huggins9000211 <39134424+huggins9000211@users.noreply.github.com>
2021-04-08 14:02:51 -0400

    test

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/GHbutt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GHbutt : MonoBehaviour
+{
+    public AudioClip ts;
+    AudioSource audioSourse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSourse = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenDiscord()
+    {
+        StartCoroutine(doShit()); 
+    }
+
+    IEnumerator doShit()
+    {
+        audioSourse.PlayOneShot(ts, 1.0f);
+        yield return new WaitForSeconds(3);
+        Application.OpenURL("https://github.com/huggins9000211");
+    }
+}
---
+++ 0x09-unity_ar_business_card/Assets/Scripts/Od.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.UI;
+
+public class Od : MonoBehaviour
+{
+    public Button dc;
+    public Button li;
+    public Button ig;
+    public Button gh;
+    public float seconds = 3;
+    public float timer = 10;
+    public Vector3 igP;
+    public Vector3 dcP;
+    public Vector3 ghP;
+    public Vector3 liP;
+    public Vector3 igDifference;
+    public Vector3 dcDifference;
+    public Vector3 ghDifference;
+    public Vector3 liDifference;
+    public Vector3 start = new Vector3(0, 0, 0);
+    public float percent ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        igP = new Vector3(8, -13.8f, 0);
+        dcP = new Vector3(14, -1.7f, 0);
+        ghP = new Vector3(-14, -1.7f, 0);
+        liP = new Vector3(-8, -13.8f, 0);
+        igDifference = igP - start;
+        ghDifference = ghP - start;
+        liDifference = liP - start;
+        dcDifference = dcP - start;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= seconds) 
+        {
+            timer += Time.deltaTime;
+            percent = timer / seconds;
+            dc.transform.position = start + dcDifference * percent;
+            ig.transform.position = start + igDifference * percent;
+            li.transform.position = start + liDifference * percent;
+            gh.transform.position = start + ghDifference * percent;
+        }
+    }
+
+    void OnTargetFound()
+    {
+        dc.transform.position = new Vector3(0, 0, 0);
+        li.transform.position = new Vector3(0, 0, 0);
+        ig.transform.position = new Vector3(0, 0, 0);
+        gh.transform.position = new Vector3(0, 0, 0);
+        timer = 0;
+    }
+}
